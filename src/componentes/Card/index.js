500b16cc058fc6da24780016295803762f1ef53d
Nikooooh <nicolastrevis@hotmail.com>
2024-03-01 01:30:23 -0300

    Corrigindo Cards de restaurantes

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -4,23 +4,23 @@ import { Link } from "react-router-dom";
 
 const CardContainer = styled.div`
   position: relative;
-  background-color: #fff;
-  width: 472px; /* Ajuste na largura do card */
-  height: 500px;
+  width: 472px;
+  height: 405px;
   margin-bottom: 20px;
   margin-top: 59px;
-  padding: 12px;
-  border: 1px solid #ffa07a;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  padding: 0;
+  border: 2px solid #e66767;
+  margin-right: 60px;
+  margin-left: 60px;
+  background-color: #ffffff;
 `;
 
 const CardImage = styled.img`
-  width: 100%; /* Ajuste na largura da imagem */
+  width: 472px;
   height: 217px;
   object-fit: cover;
-  border-radius: 0px;
+  border-radius: 0;
+  border: none;
 `;
 
 const RestaurantName = styled.h2`
@@ -28,37 +28,53 @@ const RestaurantName = styled.h2`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  font-size: 20px; /* Ajuste no tamanho da fonte */
+  font-size: 20px;
+  margin-top: -3px;
+  margin-left: 6px;
 `;
 
 const Description = styled.p`
   color: #e66767;
-  font-size: 18px;
+  font-size: 14px;
+  font-family: "Roboto", sans-serif;
+  margin-right: 2px;
+  font-style: normal;
+  font-weight: 400;
+  margin-left: 6px;
 `;
 
 const FoodType = styled.div`
   position: absolute;
-  top: 30px;
-  right: 25px;
+  top: 19px;
+  right: 15px;
   background-color: #e66767;
   color: white;
-  padding: 8px 10px;
+  padding: 6px 8px;
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+  font-weight: 500;
   border-radius: 0px;
 `;
 
 const Rating = styled.div`
   color: #ff8c00;
-  font-size: 20px;
+  font-size: 19px;
+  padding: 8px;
 `;
 
 const Button = styled.button`
   background-color: #e66767;
   color: white;
   border: none;
-  padding: 12px 24px; /* Ajuste no espaçamento interno */
+  padding: 4px 6px;
   border-radius: 1px;
+  text-align: center;
   cursor: pointer;
-  font-size: 16px; /* Ajuste no tamanho da fonte */
+  font-size: 14px;
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  margin-left: 6px;
 `;
 
 const Card = ({ image, restaurantName, description, foodType, rating, id }) => {
