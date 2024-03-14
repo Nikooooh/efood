86caa54fc27bff8feb4ef32add5ca4325352fb44
Nikooooh <nicolastrevis@hotmail.com>
2024-03-14 11:01:08 -0300

    .

--- src/componentes/Restaurant/index.js
+++ src/componentes/Restaurant/index.js
@@ -113,8 +113,8 @@ const ModalContent = styled.div`
 
 const CloseButton = styled.button`
   position: absolute;
-  top: 285px;
-  right: 475px;
+  top: 289px;
+  right: 455px;
   background-color: transparent;
   border: none;
   cursor: pointer;
