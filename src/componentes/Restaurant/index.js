f6bd0141f7d5a0f5e4db75ecfbaf631b738609b3
Nikooooh <nicolastrevis@hotmail.com>
2024-03-14 10:55:13 -0300

    Ajustes no deploy

--- src/componentes/Restaurant/index.js
+++ src/componentes/Restaurant/index.js
@@ -113,8 +113,8 @@ const ModalContent = styled.div`
 
 const CloseButton = styled.button`
   position: absolute;
-  top: 345px;
-  right: 525px;
+  top: 325px;
+  right: 505px;
   background-color: transparent;
   border: none;
   cursor: pointer;
