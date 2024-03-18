6a804c51b148439134000948c63a10942d3057a1
Nikooooh <nicolastrevis@hotmail.com>
2024-03-18 14:55:19 -0300

    .

--- src/componentes/Restaurant/index.js
+++ src/componentes/Restaurant/index.js
@@ -14,7 +14,7 @@ const MenuItemCard = styled.div`
   border: 10px solid #e66767;
   margin-bottom: -40px;
   margin-top: 70px;
-  margin-right: 70px;
+  margin-right: 61px;
 `;
 
 const CardImage = styled.img`
